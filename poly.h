a6b29a8629180b29f0e8315d1bed549e1e46426b
zhou-jk <55532673+zhou-jk@users.noreply.github.com>
2021-02-02 15:18:11 +0800

    updae poly.h

--- poly.h
+++ poly.h
@@ -8,9 +8,9 @@
 #include<functional>
 #include<algorithm>
 using namespace std;
-const int N=200005;
+const int N=4000005;
 const int g=3;
-const int MOD=1004535809;
+const int MOD=998244353;
 int ksm(int a,int b)
 {
 	int res=1;
@@ -22,6 +22,17 @@ int ksm(int a,int b)
 	return res;
 }
 int W[2][N<<1];
+void init(int n=4000000)
+{
+	for(int len=1;len<=n;len<<=1)
+	{
+		int w=ksm(::g,(MOD-1)/len),iw=ksm(w,MOD-2);
+		W[0][len]=W[1][len]=1;
+		for(int k=1;k<len;k++)
+			W[0][len+k]=1LL*W[0][len+k-1]*w%MOD,W[1][len+k]=1LL*W[1][len+k-1]*iw%MOD;
+	}
+	return;
+}
 typedef vector<int> Poly;
 Poly operator+(const Poly &a,const Poly &b)
 {
@@ -55,8 +66,6 @@ Poly ntt(const Poly &F,const Poly &G,const function<int(int,int)> &mul)
 	int n=f.size()-1,m=g.size()-1;
 	m+=n,n=1;
 	while(n<=m) n<<=1;
-	for(int i=1;i<=n;i<<=1)
-		W[0][i]=ksm(::g,(MOD-1)/i),W[1][i]=ksm(W[0][i],MOD-2);
 	f.resize(n);
 	g.resize(n);
 	vector<int>rev(n);
@@ -65,30 +74,31 @@ Poly ntt(const Poly &F,const Poly &G,const function<int(int,int)> &mul)
 		rev[i]=rev[i/2]>>1;
 		if(i&1) rev[i]|=n/2;
 	}
+	static const int BIT=17;
 	function<void(Poly &)> dft=[=](Poly &F)
 	{
 		int n=F.size();
-		Poly f(n);
+		vector<unsigned long long>f(n);
 		for(int i=0;i<n;i++)
 			f[i]=F[rev[i]];
 		for(int len=2;len<=n;len<<=1)
 		{
-			int w=W[0][len];
-			for(int i=0;i<n;i+=len)
+			if(len&(1<<BIT))
 			{
-				int buf=1;
+				for(int i=0;i<n;i++)
+					f[i]%=MOD;
+			}
+			for(int i=0;i<n;i+=len)
 				for(int k=i;k<i+len/2;k++)
 				{
-					int l=f[k],r=1LL*buf*f[k+len/2]%MOD;
+					unsigned long long l=f[k];
+					int r=W[0][len+k-i]*f[k+len/2]%MOD;
 					f[k]=l+r;
-					if(f[k]>=MOD) f[k]-=MOD;
-					f[k+len/2]=l-r;
-					if(f[k+len/2]<0) f[k+len/2]+=MOD;
-					buf=1LL*buf*w%MOD;
+					f[k+len/2]=l+MOD-r;
 				}
-			}
 		}
-		F=f;
+		for(int i=0;i<n;i++)
+			F[i]=f[i]%MOD;
 		return;
 	};
 	dft(f);
@@ -98,27 +108,27 @@ Poly ntt(const Poly &F,const Poly &G,const function<int(int,int)> &mul)
 	function<void(Poly &)> idft=[=](Poly &F)
 	{
 		int n=F.size();
-		Poly f(n);
+		vector<unsigned long long>f(n);
 		for(int i=0;i<n;i++)
 			f[i]=F[rev[i]];
 		for(int len=2;len<=n;len<<=1)
 		{
-			int w=W[1][len];
-			for(int i=0;i<n;i+=len)
+			if(len&(1<<BIT))
 			{
-				int buf=1;
+				for(int i=0;i<n;i++)
+					f[i]%=MOD;
+			}
+			for(int i=0;i<n;i+=len)
 				for(int k=i;k<i+len/2;k++)
 				{
-					int l=f[k],r=1LL*buf*f[k+len/2]%MOD;
+					unsigned long long l=f[k];
+					int r=W[1][len+k-i]*f[k+len/2]%MOD;
 					f[k]=l+r;
-					if(f[k]>=MOD) f[k]-=MOD;
-					f[k+len/2]=l-r;
-					if(f[k+len/2]<0) f[k+len/2]+=MOD;
-					buf=1LL*buf*w%MOD;
+					f[k+len/2]=l+MOD-r;
 				}
-			}
 		}
-		F=f;
+		for(int i=0;i<n;i++)
+			F[i]=f[i]%MOD;
 		return;
 	};
 	idft(f);
@@ -164,6 +174,89 @@ Poly getinv(const Poly &F)
 	}
 	g.resize(m+1);
 	return g;
+}
+int w;
+struct Complex
+{
+	int real,imag;
+	bool operator ==(const Complex &b)const
+	{
+		return real==b.real&&imag==b.imag;
+	}
+	Complex operator *(const Complex &b)const
+	{
+		Complex res;
+		res.real=(1LL*real*b.real+1LL*w*imag%MOD*b.imag)%MOD;
+		res.imag=(1LL*real*b.imag+1LL*imag*b.real)%MOD;
+		return res;
+	}
+	friend Complex ksm(Complex a,int b)
+	{
+		Complex res=(Complex){1,0};
+		while(b)
+		{
+			if(b&1) res=res*a;
+			a=a*a,b>>=1;
+		}
+		return res;
+	}
+};
+int cipolla(int n)
+{
+	static mt19937 myrand(time(NULL));
+	if(n==0) return 0;
+	function<bool(int)>check=[=](const int &n){return ksm(n,(MOD-1)/2)==1;};
+	if(!check(n)) return -1;
+	int a=myrand()%(MOD-1)+1;
+	while(check((1LL*a*a-n+MOD)%MOD)) a=myrand()%(MOD-1)+1;
+	w=(1LL*a*a-n+MOD)%MOD;
+	Complex res=ksm((Complex){a,1},(MOD+1)/2);
+	return res.real;
+}
+Poly sqrt(const Poly &F)
+{
+	Poly f=F;
+	int m=f.size()-1;
+	int n=1;
+	while(n<=m) n<<=1;
+	f.resize(n);
+	int g0=cipolla(f[0]);
+	Poly g={min(g0,MOD-g0)};
+	int inv2=ksm(2,MOD-2);
+	for(int m=2;m<=n;m<<=1)
+	{
+		Poly t(f.begin(),f.begin()+m);
+		g.resize(m);
+		g=g*inv2+ntt(t,getinv(g),[=](const int &x,const int &y){return 1LL*inv2*x%MOD*y%MOD;});
+		g.resize(m);
+	}
+	g.resize(m+1);
+	return g;
+}
+Poly operator/(const Poly &F,const Poly &G)
+{
+	Poly f=F,g=G;
+	int n=f.size()-1,m=g.size()-1;
+	if(n<m) return Poly(n-m+1);
+	reverse(f.begin(),f.end());
+	reverse(g.begin(),g.end());
+	f.resize(n-m+1);
+	g.resize(n-m+1);
+	Poly q=f*getinv(g);
+	q.resize(n-m+1);
+	reverse(q.begin(),q.end());
+	return q;
+}
+Poly operator%(const Poly &F,const Poly &G)
+{
+	Poly f=F,g=G,q=f/g;
+	int m=g.size()-1;
+	g.resize(m);
+	q.resize(m);
+	Poly c=g*q;
+	c.resize(m);
+	f.resize(m);
+	return f-c;
 }
 Poly diff_calc(const Poly &F)
 {
@@ -216,6 +309,29 @@ Poly exp(const Poly &F)
 	g.resize(m+1);
 	return g;
 }
+Poly ksm(const Poly &F,const int &k)
+{
+	Poly f=F;
+	int n=f.size()-1;
+	Poly g(n+1);
+	int pos=-1;
+	for(int i=0;i<=n;i++)
+		if(f[i]>0)
+		{
+			g[i]=f[i];
+			pos=i;
+			break;
+		}
+	if(pos==-1) return g;
+	Poly a=f*getinv(g);
+	a.resize(n+1);
+	g[pos]=0;
+	if(1LL*pos*k<=n) g[pos*k]=ksm(f[pos],k);
+	else return g;
+	g=g*exp(k*ln(a));
+	g.resize(n+1);
+	return g;
+}
 bool isbig;
 Poly ksm(const Poly &F,const int &k1,const int &k2)
 {
@@ -265,31 +381,84 @@ pair<int,int>read(const int &p1,const int &p2)
 	if(!flag) x={p1-x.first,p2-x.second};
 	return x;
 }
-int main()
+Poly poly_eval(const Poly &F,const Poly &a)
 {
-	int n,t;
-	scanf("%d",&n);
-	pair<int,int> k=read(MOD-1,MOD);
-	scanf("%d",&t);
-	Poly a(n);
-	for(int i=0;i<n;i++)
-		scanf("%d",&a[i]);
-	if(t==0)
+	Poly f=F;
+	int m=a.size();
+	vector<Poly>g(m<<2);
+	function<void(int,int,int)> init_poly_eval=[&](int i,int l,int r)
 	{
-		Poly f(n,1);
-		Poly g=a*ksm(f,k.first,k.second);
-		g.resize(n);
-		for(int i=0;i<n;i++)
-			printf("%d ",g[i]);
-	}
-	else if(t==1)
+		if(l==r)
+		{
+			g[i]=(Poly){MOD-a[l],1};
+			return;
+		}
+		int mid=(l+r)/2;
+		init_poly_eval(i*2,l,mid);
+		init_poly_eval(i*2+1,mid+1,r);
+		g[i]=g[i*2]*g[i*2+1];
+		return;
+	};
+	init_poly_eval(1,0,m-1);
+	Poly res(m);
+	function<void(int,int,int,const Poly &)> solve_poly_eval=[&](int i,int l,int r,const Poly &f)
 	{
-		Poly f={1,MOD-1};
-		f.resize(n);
-		Poly g=a*ksm(f,k.first,k.second);
-		g.resize(n);
-		for(int i=0;i<n;i++)
-			printf("%d ",g[i]);
-	}
-	return 0;
+		if(l==r)
+		{
+			res[l]=f[0];
+			return;
+		}
+		int mid=(l+r)/2;
+		solve_poly_eval(i*2,l,mid,f%g[i*2]);
+		solve_poly_eval(i*2+1,mid+1,r,f%g[i*2+1]);
+		return;
+	};
+	solve_poly_eval(1,0,m-1,f);
+	return res;
+}
+struct Point
+{
+	int x,y;
+};
+Poly poly_inte(const vector<Point> &p)
+{
+	int n=p.size()-1;
+	vector<Poly>g(n<<2);
+	function<void(int,int,int)> init_poly_eval=[&](int i,int l,int r)
+	{
+		if(l==r)
+		{
+			g[i]=(Poly){MOD-p[l].x,1};
+			return;
+		}
+		int mid=(l+r)/2;
+		init_poly_eval(i*2,l,mid);
+		init_poly_eval(i*2+1,mid+1,r);
+		g[i]=g[i*2]*g[i*2+1];
+		return;
+	};
+	init_poly_eval(1,0,n);
+	Poly x(n+1);
+	for(int i=0;i<=n;i++)
+		x[i]=p[i].x;
+	Poly F=poly_eval(diff_calc(g[1]),x);
+	vector<int> a(n+1);
+	for(int i=0;i<=n;i++)
+		a[i]=1LL*p[i].y*ksm(F[i],MOD-2)%MOD;
+	vector<Poly>res(n<<2);
+	function<void(int,int,int)> solve_poly_inte=[&](int i,int l,int r)
+	{
+		if(l==r)
+		{
+			res[i]={a[l]};
+			return;
+		}
+		int mid=(l+r)/2;
+		solve_poly_inte(i*2,l,mid);
+		solve_poly_inte(i*2+1,mid+1,r);
+		res[i]=res[i*2]*g[i*2+1]+res[i*2+1]*g[i*2];
+		return;
+	};
+	solve_poly_inte(1,0,n);
+	return res[1];
 }
